a457df708a4403c729d18a7d4d8585ba49f09a3a
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-03-28 10:38:00 +0200

    added functionality to save in the db

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Category;
 use App\Infrastructure\Curl\CurlService;
 use App\Infrastructure\Curl\Strategy\CurlGetStrategy;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,6 +20,7 @@ class BookController extends AbstractController
 
   public function __construct(
     private CurlService $curlService,
+    private EntityManagerInterface $entityManager,
   ) {
   }
 
@@ -29,10 +34,63 @@ class BookController extends AbstractController
 
   public function searchBooksByTitle(string $title): Response
   {
+    $bookRepository = $this->entityManager->getRepository(Book::class);
+    $book = $bookRepository->findOneBy(['title' => $title]);
+
+    if ($book) {
+      // Book found in the database, return the book information
+      return $this->json(['result' => $book]);
+    }
+
     $apiKey = getenv('BOOKS_APP_API_KEY');
     $url = "https://www.googleapis.com/books/v1/volumes?q=?+intitle:$title&key=$apiKey";
     $result = $this->curlService->setStrategy(new CurlGetStrategy())->setUrl($url)->doRequest();
 
-    return $this->json(['result' => json_decode($result)]);
+    $data = json_decode($result, true);
+
+    // Save the book in the database
+    $bookData = $data['items'][0];
+    $book = new Book();
+    $book->setId($bookData['id']);
+    $book->setTitle($bookData['volumeInfo']['title']);
+    $book->setPublisher($bookData['volumeInfo']['publisher'] ?? '');
+    $book->setPublishedDate($bookData['volumeInfo']['publishedDate'] ?? '');
+    $book->setDescription($bookData['volumeInfo']['description'] ?? '');
+    $book->setPageCount($bookData['volumeInfo']['pageCount'] ?? 0);
+    $book->setAverageRating($bookData['volumeInfo']['averageRating'] ?? 0);
+    $book->setRatingsCount($bookData['volumeInfo']['ratingsCount'] ?? 0);
+    $book->setMaturityRating($bookData['volumeInfo']['maturityRating'] ?? '');
+    $book->setThumbnailUrl($bookData['volumeInfo']['imageLinks']['thumbnail']?? '');
+    $book->setLanguageCode($bookData['volumeInfo']['language'] ?? '');
+    $book->setQuantity(5);
+    $book->setNumAvailable(5);
+
+    // Save categories
+    foreach ($bookData['volumeInfo']['categories'] ?? [] as $categoryName) {
+      $category = $this->entityManager->getRepository(Category::class)->findOneBy(['categoryName' => $categoryName]);
+      if (!$category) {
+        $category = new Category();
+        $category->setCategoryName($categoryName);
+        $this->entityManager->persist($category);
+      }
+      $book->addCategory($category);
+    }
+
+    // Save authors
+    foreach ($bookData['volumeInfo']['authors'] ?? [] as $authorName) {
+      $author = $this->entityManager->getRepository(Author::class)->findOneBy(['lastName' => $authorName]);
+      if (!$author) {
+        $author = new Author();
+        $author->setLastName($authorName);
+        $this->entityManager->persist($author);
+      }
+      $book->addAuthor($author);
+    }
+
+    $this->entityManager->persist($book);
+
+    $this->entityManager->flush();
+
+    return $this->json(['result' => $book]);
   }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,133 +10,136 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+  #[ORM\Id]
+  #[ORM\GeneratedValue]
+  #[ORM\Column]
+  private ?int $id = null;
 
-    #[ORM\Column(length: 20)]
-    private ?string $username = null;
+  #[ORM\Column(length: 20)]
+  private ?string $username = null;
 
-    #[ORM\Column(length: 30)]
-    private ?string $firstName = null;
+  #[ORM\Column(length: 30)]
+  private ?string $firstName = null;
 
-    #[ORM\Column(length: 30)]
-    private ?string $lastName = null;
+  #[ORM\Column(length: 30)]
+  private ?string $lastName = null;
 
-    #[ORM\Column(length: 10)]
-    private ?string $registrationDate = null;
+  #[ORM\Column(length: 10)]
+  private ?string $registrationDate = null;
 
-    #[ORM\ManyToMany(targetEntity: Book::class)]
-    private Collection $favorites;
+#[ORM\ManyToMany(targetEntity: Book::class)]
+#[ORM\JoinTable(name: 'user_favoriteBooks')]
+private Collection $favorites;
 
-    #[ORM\ManyToMany(targetEntity: Book::class)]
-    private Collection $borrowedBooks;
+#[ORM\ManyToMany(targetEntity: Book::class)]
 
-    public function __construct()
-    {
-        $this->favorites = new ArrayCollection();
-        $this->borrowedBooks = new ArrayCollection();
+#[ORM\JoinTable(name: 'user_borrowedBooks')]
+private Collection $borrowedBooks;
+
+  public function __construct()
+  {
+    $this->favorites = new ArrayCollection();
+    $this->borrowedBooks = new ArrayCollection();
+  }
+
+  public function getId(): ?int
+  {
+    return $this->id;
+  }
+
+  public function getUsername(): ?string
+  {
+    return $this->username;
+  }
+
+  public function setUsername(string $username): self
+  {
+    $this->username = $username;
+
+    return $this;
+  }
+
+  public function getFirstName(): ?string
+  {
+    return $this->firstName;
+  }
+
+  public function setFirstName(string $firstName): self
+  {
+    $this->firstName = $firstName;
+
+    return $this;
+  }
+
+  public function getLastName(): ?string
+  {
+    return $this->lastName;
+  }
+
+  public function setLastName(string $lastName): self
+  {
+    $this->lastName = $lastName;
+
+    return $this;
+  }
+
+  public function getRegistrationDate(): ?string
+  {
+    return $this->registrationDate;
+  }
+
+  public function setRegistrationDate(string $registrationDate): self
+  {
+    $this->registrationDate = $registrationDate;
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Book>
+   */
+  public function getFavorites(): Collection
+  {
+    return $this->favorites;
+  }
+
+  public function addFavorite(Book $favorite): self
+  {
+    if (!$this->favorites->contains($favorite)) {
+      $this->favorites->add($favorite);
     }
 
-    public function getId(): ?int
-    {
-        return $this->id;
+    return $this;
+  }
+
+  public function removeFavorite(Book $favorite): self
+  {
+    $this->favorites->removeElement($favorite);
+
+    return $this;
+  }
+
+  /**
+   * @return Collection<int, Book>
+   */
+  public function getBorrowedBooks(): Collection
+  {
+    return $this->borrowedBooks;
+  }
+
+  public function addBorrowedBook(Book $borrowedBook): self
+  {
+    if (!$this->borrowedBooks->contains($borrowedBook)) {
+      $this->borrowedBooks->add($borrowedBook);
     }
 
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
+    return $this;
+  }
 
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
+  public function removeBorrowedBook(Book $borrowedBook): self
+  {
+    $this->borrowedBooks->removeElement($borrowedBook);
 
-        return $this;
-    }
-
-    public function getFirstName(): ?string
-    {
-        return $this->firstName;
-    }
-
-    public function setFirstName(string $firstName): self
-    {
-        $this->firstName = $firstName;
-
-        return $this;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(string $lastName): self
-    {
-        $this->lastName = $lastName;
-
-        return $this;
-    }
-
-    public function getRegistrationDate(): ?string
-    {
-        return $this->registrationDate;
-    }
-
-    public function setRegistrationDate(string $registrationDate): self
-    {
-        $this->registrationDate = $registrationDate;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Book>
-     */
-    public function getFavorites(): Collection
-    {
-        return $this->favorites;
-    }
-
-    public function addFavorite(Book $favorite): self
-    {
-        if (!$this->favorites->contains($favorite)) {
-            $this->favorites->add($favorite);
-        }
-
-        return $this;
-    }
-
-    public function removeFavorite(Book $favorite): self
-    {
-        $this->favorites->removeElement($favorite);
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, Book>
-     */
-    public function getBorrowedBooks(): Collection
-    {
-        return $this->borrowedBooks;
-    }
-
-    public function addBorrowedBook(Book $borrowedBook): self
-    {
-        if (!$this->borrowedBooks->contains($borrowedBook)) {
-            $this->borrowedBooks->add($borrowedBook);
-        }
-
-        return $this;
-    }
-
-    public function removeBorrowedBook(Book $borrowedBook): self
-    {
-        $this->borrowedBooks->removeElement($borrowedBook);
-
-        return $this;
-    }
+    return $this;
+  }
 }

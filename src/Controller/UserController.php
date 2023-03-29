076b0505aa18514473b47d3e43a99c41870bf22e
Feld, Naomi <Naomi.Feld@adesso.de>
2023-03-29 11:14:50 +0200

    create UserController

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+    public function displayFavorites(User $user) {
+        
+    }
+}

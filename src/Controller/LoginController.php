80a604c5cddf22aef2d1b965f2df54fc214d774e
Anna Lepak <anna.lepak@adesso.de>
2023-03-30 15:36:23 +0200

    added login and logout logic

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class LoginController extends AbstractController {
+    #[Route(path: '/login', name: 'api_login')]
+    public function index(#[CurrentUser] ?User $user) : Response {
+        if ($user === null) {
+            return $this->json(['message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json(['message' => 'Welcome ' . $user->getUserIdentifier() .  '!']);
+    }
+}

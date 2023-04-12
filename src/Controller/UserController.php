1c7effbb66d725e1a2b3d5943cdecc8ca2f25c4e
Pashkovska, Oksana <Oksana.Pashkovska@adesso.de>
2023-04-12 14:18:49 +0200

    corrected returned value

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -40,17 +40,17 @@ class UserController extends AbstractController
   public function addFavoriteBook(#[CurrentUser] ?User $user, string $id): Response
   {
     if ($user === null) {
-      return $this->json(['message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
+      return $this->json(['success' => false, 'message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
     }
 
     $book = $this->bookDBService->searchBookById($id);
 
     if ($book === null) {
-      return $this->json(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+      return $this->json(['success' => false, 'message' => 'Book not found'], Response::HTTP_NOT_FOUND);
     }
 
     if ($user->getFavorites()->contains($book)) {
-      return $this->json(['message' => 'Book already in favorites'], Response::HTTP_BAD_REQUEST);
+      return $this->json(['success' => false, 'message' => 'Book is already in favorites'], Response::HTTP_BAD_REQUEST);
     }
 
     $user->addFavorite($book);
@@ -58,23 +58,23 @@ class UserController extends AbstractController
     $this->entityManager->persist($user);
     $this->entityManager->flush();
 
-    return $this->json(['success' => true, 'data' => $book]);
+    return $this->json(['success' => true, 'favoriteList' => $user->getFavorites()]);
   }
 
-  public function removeFavoriteBook(#[CurrentUser] ?User $user, string $id): Response 
+  public function removeFavoriteBook(#[CurrentUser] ?User $user, string $id): Response
   {
     if ($user === null) {
-      return $this->json(['message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
+      return $this->json(['success' => false, 'message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
     }
 
     $book = $this->bookDBService->searchBookById($id);
 
     if ($book === null) {
-      return $this->json(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+      return $this->json(['success' => false, 'message' => 'Book not found'], Response::HTTP_NOT_FOUND);
     }
 
     if (!$user->getFavorites()->contains($book)) {
-      return $this->json(['message' => 'Book is not in favorites'], Response::HTTP_BAD_REQUEST);
+      return $this->json(['success' => false, 'message' => 'Book is not in favorites'], Response::HTTP_BAD_REQUEST);
     }
 
     $user->removeFavorite($book);
@@ -82,7 +82,6 @@ class UserController extends AbstractController
     $this->entityManager->persist($user);
     $this->entityManager->flush();
 
-    return $this->json(['success' => true, 'data' => $book]);
-
+    return $this->json(['success' => true, 'favoriteList' => $user->getFavorites()]);
   }
 }

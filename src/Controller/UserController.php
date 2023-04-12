14aac64dbe151bfcbdd3d2b5202254d75fa9d3b9
Anna Lepak <anna.lepak@adesso.de>
2023-04-12 13:54:30 +0200

    created addBorrowedBooks method

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -37,7 +37,7 @@ class UserController extends AbstractController
     return $this->json(['borrowed' => $user->getBorrowedBooks()]);
   }
 
-  public function addFavoriteBook(#[CurrentUser] ?User $user, string $id): Response
+  public function addFavoriteBook(string $id, #[CurrentUser] ?User $user): Response
   {
     if ($user === null) {
       return $this->json(['message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
@@ -46,11 +46,11 @@ class UserController extends AbstractController
     $book = $this->bookDBService->searchBookById($id);
 
     if ($book === null) {
-      return $this->json(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+      return $this->json(['success' => false, 'message' => 'Book not found'], Response::HTTP_NOT_FOUND);
     }
 
     if ($user->getFavorites()->contains($book)) {
-      return $this->json(['message' => 'Book already in favorites'], Response::HTTP_BAD_REQUEST);
+      return $this->json(['success' => false, 'message' => 'Book already in favorites'], Response::HTTP_BAD_REQUEST);
     }
 
     $user->addFavorite($book);
@@ -58,6 +58,35 @@ class UserController extends AbstractController
     $this->entityManager->persist($user);
     $this->entityManager->flush();
 
-    return $this->json(['success' => true, 'data' => $book]);
+    return $this->json(['success' => true, 'favoritesList' => $user->getFavorites()]);
+  }
+
+  public function addBorrowedBook(string $id, #[CurrentUser] ?User $user) {
+    if (!$user) {
+      return $this->json(['success' => false, 'message' => 'missing or wrong credentials'], Response::HTTP_UNAUTHORIZED);
+    }
+
+    $book = $this->bookDBService->searchBookById($id);
+    if(!$book) {
+      return $this->json(['message' => 'Book not found'], Response::HTTP_NOT_FOUND);
+    }
+    if ($user->getBorrowedBooks()->contains($book)) {
+      return $this->json(['success' => false, 'message' => 'Book is already borrowed by the user'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $numAvailable = $book->getNumAvailable();
+    if($numAvailable <= 0) {
+      return $this->json(['success' => false, 'message' => 'No Books available to borrow'], Response::HTTP_BAD_REQUEST);
+    }
+
+    $user->addBorrowedBook($book);
+    $this->entityManager->persist($user);
+    
+    $book->setNumAvailable($numAvailable - 1);
+    $this->entityManager->persist($book);
+    
+    $this->entityManager->flush();
+
+    return $this->json(['success' => true, 'borrowedList' => $user->getBorrowedBooks()]);
   }
 }
